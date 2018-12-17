2b687f7849053b5daa3a98f53e024d4b47038dfa
MrFlynn <nick@pleatsikas.me>
2018-12-16 17:42:11 -0800

    Replaced word randomization code.
    Replaced brute for appending code with iterative  method instead. Added
    logic that decides what kind of words to add (synonyms, related, or
    similar words). Finally, added list of words to not look up (mostly
    pronouns, prepositions, and other "connecting" words).

--- pkg/sentence/sentence.go
+++ pkg/sentence/sentence.go
@@ -2,6 +2,7 @@ package sentence
 
 import (
 	"math/rand"
+	"reflect"
 	"regexp"
 	"strings"
 	"time"
@@ -12,48 +13,68 @@ import (
 // ThesaurizeSentence takes a sentence of words and replaces each with a related word.
 func ThesaurizeSentence(sentence string, api thesaurus.API) (string, error) {
 	regx, _ := regexp.Compile("[^a-zA-Z0-9]")
-	s := rand.NewSource(time.Now().Unix())
-	r := rand.New(s)
+	randomizer := rand.New(rand.NewSource(time.Now().Unix()))
 
 	stringArray := strings.Split(sentence, " ")
 	output := make([]string, 0, len(stringArray))
 
 	for i := range stringArray {
 		word := regx.ReplaceAllString(stringArray[i], "")
-		synonymResponse, _ := api.Call(word)
 
-		words := collectWords(synonymResponse)
-
-		// If nothing could be appended
-		if len(words) == 0 {
+		if _, ok := ignoreWords[word]; ok {
 			output = append(output, word)
 		} else {
-			idx := r.Intn(len(words))
-			output = append(output, words[idx])
+			resp, _ := api.Call(word)
+			words := compileWordBucket(resp)
+
+			if len(words) == 0 {
+				// If thesaurus could not find synonym then return the input word.
+				output = append(output, word)
+			} else {
+				idx := randomizer.Intn(len(words))
+				output = append(output, words[idx])
+			}
 		}
 	}
 
 	return strings.Join(output, " "), nil
 }
 
-func collectWords(resp thesaurus.Response) []string {
+// Compile list of synonyms, related words, etc. that will be used to randomly
+// draw from later.
+func compileWordBucket(resp thesaurus.Response) []string {
 	words := make([]string, 0, 10)
 
-	words = append(words, resp.Adjective.Related...)
-	words = append(words, resp.Adjective.Similar...)
-	words = append(words, resp.Adjective.Synonym...)
+	v := reflect.ValueOf(resp)
+	for i := 0; i < v.NumField(); i++ {
+		element := v.Field(i)
 
-	words = append(words, resp.Adverb.Related...)
-	words = append(words, resp.Adverb.Similar...)
-	words = append(words, resp.Adverb.Synonym...)
+		if element.CanInterface() {
+			entity := element.Interface()
 
-	words = append(words, resp.Noun.Related...)
-	words = append(words, resp.Noun.Similar...)
-	words = append(words, resp.Noun.Synonym...)
-
-	words = append(words, resp.Verb.Related...)
-	words = append(words, resp.Verb.Similar...)
-	words = append(words, resp.Verb.Synonym...)
+			// Type cast element to WordClass.
+			w, ok := entity.(thesaurus.WordClass)
+			if ok {
+				words = decideListAppend(w, words)
+			}
+		}
+	}
 
 	return words
+}
+
+// Decides which related words to add to bucket to be picked from later.
+func decideListAppend(class thesaurus.WordClass, list []string) []string {
+	if class.Synonym != nil {
+		list = append(list, class.Synonym...)
+		return list
+	} else if class.Similar != nil && len(list) == 0 {
+		list = append(list, class.Similar...)
+		return list
+	} else if class.Related != nil && len(list) == 0 {
+		list = append(list, class.Related...)
+		return list
+	} else {
+		return list
+	}
 }
