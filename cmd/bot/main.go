07b8005714dd736c2bee02b8893e29cece8445ab
MrFlynn <nick@pleatsikas.me>
2020-01-01 21:09:51 -0800

    Added new main function.
    Includes cli/v2 library for parsing command line arguments instead of
    using env variables.

---
+++ cmd/bot/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+
+	"github.com/MrFlynn/thesaurize-bot/internal/discord"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name: "thesaurize",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "token",
+				Aliases:  []string{"-t"},
+				Usage:    "Discord API key.",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "datastore",
+				Aliases:  []string{"-s"},
+				Usage:    "URI of Redis datastore. Formatted like redis://<address>:<port>",
+				Required: true,
+			},
+		},
+		Action: discord.Run,
+		Authors: []*cli.Author{
+			&cli.Author{
+				Name:  "Nick Pleatsikas",
+				Email: "nick@pleatsikas.me",
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/bwmarrin/discordgo v0.20.2
 	github.com/go-redis/redis/v7 v7.0.0-beta.4
+	github.com/urfave/cli/v2 v2.1.1
 )
