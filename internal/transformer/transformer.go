4558900a7108a5b799327f412fe9a3a73cd10067
MrFlynn <nick@pleatsikas.me>
2020-06-14 20:30:42 -0700

    Refactored Transform method to use new backend.

--- internal/transformer/transformer.go
+++ internal/transformer/transformer.go
@@ -2,15 +2,14 @@ package transformer
 
 import "github.com/MrFlynn/thesaurize-bot/internal/database"
 
-// Transform takes a sentence and runs each word through the thesaurus.
-func Transform(sentence string, db database.Database) string {
-	words := generateMetadataFromSentence(sentence)
+// Transform takes a message and runs each word through the thesaurus.
+func Transform(message string, db database.Database) string {
+	messageMeta := MessageMetadata{}
+	messageMeta.New(message)
 
-	for _, meta := range words {
-		if _, ok := ignoreWords[meta.Word]; !ok {
-			meta.Word = db.GetBestCandidateWord(meta.Word)
-		}
+	for idx, word := range messageMeta.Words {
+		messageMeta.Words[idx] = db.GetBestCandidateWord(word)
 	}
 
-	return constructSentence(words)
+	return messageMeta.String()
 }
